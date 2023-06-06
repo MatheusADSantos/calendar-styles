b88b0365ca45c65447e996e71f6a7d80f80fb4da
Matheus Santos <matheus.santos@ftdata.com.br>
2023-06-06 15:23:52 -0300

    Setup Binding and load calendarView

--- app/src/main/java/com/example/calendarstyles/ui/MainActivity.kt
+++ app/src/main/java/com/example/calendarstyles/ui/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.calendarstyles.ui
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.calendarstyles.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupBinding()
+        setupCalendar()
+    }
+
+    private fun setupBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun setupCalendar() {
+        binding.calendarView
     }
 }
