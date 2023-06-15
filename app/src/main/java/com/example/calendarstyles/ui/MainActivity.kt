6a3e5767a89b246eef780ad79713367571d30d5d
Matheus Santos <matheus.santos@ftdata.com.br>
2023-06-15 12:03:10 -0300

    Add listeners to button and when touch calendar

--- app/src/main/java/com/example/calendarstyles/ui/MainActivity.kt
+++ app/src/main/java/com/example/calendarstyles/ui/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.example.calendarstyles.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calendarstyles.R
 import com.example.calendarstyles.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
 
 class MainActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var calendar: Calendar
+    private lateinit var calendarView: CalendarView
+    private lateinit var labelDate: TextView
+    private lateinit var buttonChangeStyle: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupBinding()
         setupCalendar()
+        setupButtonStyle()
     }
 
     private fun setupBinding() {
@@ -22,7 +39,29 @@ class MainActivity: AppCompatActivity() {
     }
 
     private fun setupCalendar() {
-        binding.calendarView
-//        binding.constraint.setBackgroundColor()
+        calendarView = binding.calendarView
+        labelDate = binding.descriptionData
+        labelDate.text = Calendar.DATE.toString()
+        calendar = Calendar.getInstance()
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val message = "$dayOfMonth/$month/$year"
+            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+            labelDate.text = message
+        }
     }
+
+    private fun setupButtonStyle() {
+        buttonChangeStyle = binding.buttonSelect
+        buttonChangeStyle.setOnClickListener {
+            calendarView.dateTextAppearance = com.google.android.material.R.style.TextAppearance_AppCompat_SearchResult_Subtitle
+        }
+    }
+
+    fun getToday(): String {
+        val sdf = SimpleDateFormat("dd 'de' MMMM 'de' yyyy")
+        val date = Date()
+        return sdf.format(date)
+    }
+
 }
